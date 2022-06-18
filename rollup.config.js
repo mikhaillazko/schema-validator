8965e77a5e5235d029531950691444c142473aa6
mikhaillazko <mikhail.lazko@quantumsoft.ru>
2022-06-18 14:54:37 +0700

    Add build step and remove test flag for publish

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,28 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
-import optimizeLodashImports from "rollup-plugin-optimize-lodash-imports";
-import pkg from './package.json';
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+import optimizeLodashImports from "rollup-plugin-optimize-lodash-imports"
+import pkg from './package.json'
 
-const entrypoint = './src/index.js';
+const entrypoint = './src/index.js'
 
 export default [
     {
         input: entrypoint,
-        output: {
-            name: pkg.name,
-            file: pkg.browser,
-            format: 'umd',
-            sourcemap: true
-        },
+        output: [
+            {
+                name: pkg.name,
+                file: pkg.browser,
+                format: 'umd',
+                sourcemap: true
+            },
+            {
+                name: pkg.name,
+                file: pkg.module,
+                format: 'esm',
+                sourcemap: true
+            }
+        ],
         plugins: [
             resolve(),
             optimizeLodashImports(),
@@ -31,4 +39,4 @@ export default [
             format: 'es'
         }
     }
-];
+]
